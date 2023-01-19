c5adf176bf1aa213e8f24694672aea3e02860125
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-19 17:36:52 -0500

    created the nav links with a logo

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -12,7 +12,28 @@ const Navbar = (props: Props) => {
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-        
+
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* {left} */}
+            <img src={Logo} alt="company logo" />
+
+            {/* {Right} */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Benefits</p>
+                <p>Our Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   )
