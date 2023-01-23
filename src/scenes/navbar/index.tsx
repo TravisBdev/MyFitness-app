20cca001e0a759fd53db3a98bc622a41bbf15e77
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-23 11:41:20 -0500

    formatted the links in the Navbar index

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid'
 import Logo from '@/assets/Logo.png'
+import Link from "./Link"
 
 
 type Props = {}
@@ -22,10 +23,26 @@ const Navbar = (props: Props) => {
             {/* {Right} */}
             <div className={`${flexBetween} w-full`}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact Us</p>
+                <Link 
+                  page='Home' 
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page='Benefits'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page='Our Classes'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link 
+                  page='Contact US'
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
