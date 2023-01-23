ebe963111812db37fe246eae4a90e9230f18674b
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-23 13:35:57 -0500

    added action button to NavBar for easy navigation to the contact page

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -54,7 +54,7 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                 </div>
               </div>
               ): (
