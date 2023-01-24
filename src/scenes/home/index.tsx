640fc393286bfbebe8362b130e1eef5ed207391f
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2023-01-24 15:36:19 -0500

    created the structure for the main heading - added action button to lead user to contact page on click

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,12 +1,59 @@
+import useMediaQuery from '@/hooks/UseMediaQuery'
 import { SelectedPage } from '@/shared/types'
+import ActionButton from '@/shared/ActionButton'
+import HomePageText from '@/assets/HomePageText.png'
+import HomePageGraphic from '@/assets/HomePageGraphic.png'
+import SponsorRedBull from '@/assets/SponsorRedBull.png'
+import SponsorFortune from '@/assets/SponsorFortune.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
 
 function Home({setSelectedPage}: Props) {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)")
+
   return (
-    <div>Home</div>
+    <section 
+    id='home'
+    className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
+    >
+      {/* {IMG & Main Header} */}
+      <div>
+        {/* {Main Header} */}
+        <div>
+          {/* {Headings} */}
+          <div>
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-text" />
+              </div>
+            </div>
+
+            <p>
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
+            </p>
+          </div>
+          {/* {Actions} */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+            className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+
+        {/* {IMG} */}
+        <div></div>
+      </div>
+    </section>
   )
 }
 
